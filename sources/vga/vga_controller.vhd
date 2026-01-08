e926e4f5aaf2e9c0247c7991cbd86c2ab11a79a7
CharrierTim <timothee.charrier@bbox.fr>
2026-01-08 16:05:02 +0100

    feat: add clock docmian crossing of input manual colors from system clock to VGA clock

--- sources/vga/vga_controller.vhd
+++ sources/vga/vga_controller.vhd
@@ -41,6 +41,8 @@ library ieee;
     use ieee.numeric_std.all;
     use ieee.math_real.all;
 
+library olo;
+
 library lib_rtl;
 
 -- =====================================================================================================================
@@ -50,20 +52,22 @@ library lib_rtl;
 entity VGA_CONTROLLER is
     generic (
         -- Horizontal timings (default: 640x480@60Hz)
-        G_H_PIXELS      : integer := 640;
-        G_H_FRONT_PORCH : integer := 16;
-        G_H_SYNC_PULSE  : integer := 96;
-        G_H_BACK_PORCH  : integer := 48;
+        G_H_PIXELS      : positive := 640;
+        G_H_FRONT_PORCH : positive := 16;
+        G_H_SYNC_PULSE  : positive := 96;
+        G_H_BACK_PORCH  : positive := 48;
 
         -- Vertical timings (default: 640x480@60Hz)
-        G_V_PIXELS      : integer := 480;
-        G_V_FRONT_PORCH : integer := 10;
-        G_V_SYNC_PULSE  : integer := 2;
-        G_V_BACK_PORCH  : integer := 33
+        G_V_PIXELS      : positive := 480;
+        G_V_FRONT_PORCH : positive := 10;
+        G_V_SYNC_PULSE  : positive := 2;
+        G_V_BACK_PORCH  : positive := 33
     );
     port (
-        CLK            : in    std_logic;
+        CLK_SYS        : in    std_logic;
+        CLK_VGA        : in    std_logic;
         RST_N          : in    std_logic;
+        RST_H          : in    std_logic;
 
         -- Sync outputs
         O_HSYNC        : out   std_logic;
@@ -96,8 +100,16 @@ architecture VGA_CONTROLLER_ARCH of VGA_CONTROLLER is
     -- CONSTANTS
     -- =================================================================================================================
 
+    -- Input color vector width
+    constant C_MANUAL_COLOR_VECTOR_WIDTH : positive :=
+    (
+        I_MANUAL_RED'length   +
+        I_MANUAL_GREEN'length +
+        I_MANUAL_BLUE'length
+    );
+
     -- Total horizontal pixels
-    constant C_HORIZONTAL_TOTAL_PIXELS : integer :=
+    constant C_HORIZONTAL_TOTAL_PIXELS   : positive :=
     (
         G_H_PIXELS      +
         G_H_FRONT_PORCH +
@@ -106,7 +118,7 @@ architecture VGA_CONTROLLER_ARCH of VGA_CONTROLLER is
     );
 
     -- Total vertical pixels
-    constant C_VERTICAL_TOTAL_PIXELS   : integer :=
+    constant C_VERTICAL_TOTAL_PIXELS     : positive :=
     (
         G_V_PIXELS      +
         G_V_FRONT_PORCH +
@@ -118,56 +130,82 @@ architecture VGA_CONTROLLER_ARCH of VGA_CONTROLLER is
     -- VGA horizontal and vertical active start and stop
     --
 
-    constant C_HORIZONTAL_ACTIVE_START : integer :=
+    constant C_HORIZONTAL_ACTIVE_START   : positive :=
     (
         G_H_SYNC_PULSE +
         G_H_BACK_PORCH
     );
 
-    constant C_HORIZONTAL_ACTIVE_STOP  : integer :=
+    constant C_HORIZONTAL_ACTIVE_STOP    : positive :=
     (
         C_HORIZONTAL_ACTIVE_START +
         G_H_PIXELS
     );
 
-    constant C_VERTICAL_ACTIVE_START   : integer :=
+    constant C_VERTICAL_ACTIVE_START     : positive :=
     (
         G_V_SYNC_PULSE +
         G_V_BACK_PORCH
     );
 
-    constant C_VERTICAL_ACTIVE_STOP    : integer :=
+    constant C_VERTICAL_ACTIVE_STOP      : positive :=
     (
         C_VERTICAL_ACTIVE_START +
         G_V_PIXELS
     );
 
     -- Counter width
-    constant C_HORIZONTAL_COUNT_WIDTH  : integer := integer(ceil(log2(real(C_HORIZONTAL_TOTAL_PIXELS))));
-    constant C_VERTICAL_COUNT_WIDTH    : integer := integer(ceil(log2(real(C_VERTICAL_TOTAL_PIXELS))));
+    constant C_HORIZONTAL_COUNT_WIDTH    : positive := positive(ceil(log2(real(C_HORIZONTAL_TOTAL_PIXELS))));
+    constant C_VERTICAL_COUNT_WIDTH      : positive := positive(ceil(log2(real(C_VERTICAL_TOTAL_PIXELS))));
 
     -- =================================================================================================================
     -- SIGNALS
     -- =================================================================================================================
 
+    -- Clock domain crossing for input colors
+    signal manual_colors_tosync          : std_logic_vector(C_MANUAL_COLOR_VECTOR_WIDTH - 1 downto 0);
+    signal manual_colors_resync          : std_logic_vector(C_MANUAL_COLOR_VECTOR_WIDTH - 1 downto 0);
+
     -- Counters
-    signal horizontal_count            : unsigned(C_HORIZONTAL_COUNT_WIDTH - 1 downto 0);
-    signal vertical_count              : unsigned(C_VERTICAL_COUNT_WIDTH - 1 downto 0);
+    signal horizontal_count              : unsigned(C_HORIZONTAL_COUNT_WIDTH - 1 downto 0);
+    signal vertical_count                : unsigned(C_VERTICAL_COUNT_WIDTH - 1 downto 0);
 
     -- VGA signals
-    signal active_region               : std_logic;
+    signal active_region                 : std_logic;
 
     -- Position signals
-    signal h_position                  : unsigned(C_HORIZONTAL_COUNT_WIDTH - 1 downto 0);
-    signal v_position                  : unsigned(C_VERTICAL_COUNT_WIDTH - 1 downto 0);
+    signal h_position                    : unsigned(C_HORIZONTAL_COUNT_WIDTH - 1 downto 0);
+    signal v_position                    : unsigned(C_VERTICAL_COUNT_WIDTH - 1 downto 0);
 
 begin
+
+    -- =================================================================================================================
+    -- CLOCK DOMAIN CROSSING FOR INPUT COLORS
+    -- =================================================================================================================
+
+    manual_colors_tosync <= (I_MANUAL_RED & I_MANUAL_GREEN & I_MANUAL_BLUE);
+
+    inst_olo_base_cc_status : entity olo.olo_base_cc_status
+        generic map (
+            WIDTH_G      => manual_colors_resync'length,
+            SYNCSTAGES_G => 2
+        )
+        port map (
+            In_Clk     => CLK_SYS,
+            In_RstIn   => RST_H,
+            In_RstOut  => open,
+            In_Data    => manual_colors_tosync,
+            Out_Clk    => CLK_VGA,
+            Out_RstIn  => RST_N,
+            Out_RstOut => open,
+            Out_Data   => manual_colors_resync
+        );
 
     -- =================================================================================================================
     -- HORIZONTAL AND VERTICAL COUNTERS
     -- =================================================================================================================
 
-    p_counters : process (CLK, RST_N) is
+    p_counters : process (CLK_VGA, RST_N) is
     begin
 
         if (RST_N = '0') then
@@ -175,7 +213,7 @@ begin
             horizontal_count <= (others => '0');
             vertical_count   <= (others => '0');
 
-        elsif rising_edge(CLK) then
+        elsif rising_edge(CLK_VGA) then
 
             -- Horizontal counter
             if (horizontal_count >= C_HORIZONTAL_TOTAL_PIXELS - 1) then
@@ -219,7 +257,7 @@ begin
     -- OUTPUTS
     -- =================================================================================================================
 
-    p_outputs : process (CLK, RST_N) is
+    p_outputs : process (CLK_VGA, RST_N) is
     begin
 
         if (RST_N = '0') then
@@ -235,7 +273,7 @@ begin
             O_H_POSITION <= (others => '0');
             O_V_POSITION <= (others => '0');
 
-        elsif rising_edge(CLK) then
+        elsif rising_edge(CLK_VGA) then
 
             -- Apply horizontal and vertical sync
             O_HSYNC <= '1' when horizontal_count >= G_H_SYNC_PULSE else '0';
@@ -245,9 +283,9 @@ begin
             O_ACTIVE <= active_region;
 
             -- Color outputs with blanking
-            O_RED   <= I_MANUAL_RED   when active_region = '1' else (others => '0');
-            O_GREEN <= I_MANUAL_GREEN when active_region = '1' else (others => '0');
-            O_BLUE  <= I_MANUAL_BLUE  when active_region = '1' else (others => '0');
+            O_RED   <= manual_colors_resync(11 downto 8) when active_region = '1' else (others => '0');
+            O_GREEN <= manual_colors_resync( 7 downto 4) when active_region = '1' else (others => '0');
+            O_BLUE  <= manual_colors_resync( 3 downto 0) when active_region = '1' else (others => '0');
 
             -- Position outputs
             O_H_POSITION <= std_logic_vector(resize(h_position, 16));
